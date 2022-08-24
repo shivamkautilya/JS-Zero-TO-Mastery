9a332de8e0b2734acb89f64f1ef6c3e7e7978b95
Shivam Kautilya <57669336+shivamkautilya@users.noreply.github.com>
2022-08-24 20:19:50 +0530

    Complete LECTURE: Introduction to Objects &  Dot vs. Bracket Notation  INSIDE practise/02... folder.

--- Practice/02-JS Fundamentals Part 2/script.js
+++ Practice/02-JS Fundamentals Part 2/script.js
@@ -12,3 +12,50 @@ const ageGenelia = calcAge(testBirthYear[4]);
 const calculatedAge = [ageShivam, ageMonu, ageJonas, ageBob, ageGenelia];
 console.log(calculatedAge);
 console.log(`Length of Array is ${calculatedAge.length}.`);
+
+// LECTURE: Introduction to Objects
+/*var myDetails = {
+  //example of object
+  firstName: "Shivam",
+  lastName: "Kautilya",
+  age: 2022 - 2006,
+  job: "Student",
+  friends: ["Monu", "Rishabh", "Harsh", "Priiyanshu"],
+};
+*/
+//LECTURE: Dot vs. Bracket Notation
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+};
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas["lastName"]);
+
+const nameKey = "Name";
+console.log(jonas["first" + nameKey]);
+console.log(jonas["last" + nameKey]);
+
+// console.log(jonas.'last' + nameKey)
+/*
+const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends');
+
+if (jonas[interestedIn]) {
+  console.log(jonas[interestedIn]);
+} else {
+  console.log('Wrong request! Choose between firstName, lastName, age, job, and friends');
+}
+*/
+jonas.location = "Portugal";
+jonas["twitter"] = "@jonasschmedtman";
+console.log(jonas);
+//CHALLENGE IN VIDEO
+console.log(
+  `${jonas.firstName} has ${[
+    jonas.friends.length,
+  ]} friends, and his bestfriend is ${[jonas.friends[0]]}.`
+);
