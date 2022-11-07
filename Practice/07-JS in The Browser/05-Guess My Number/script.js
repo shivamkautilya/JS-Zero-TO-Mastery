6b3b01cf0facda61f7af13d38b161ec8c195b2b1
Shivam Kautilya <57669336+shivamkautilya@users.noreply.github.com>
2022-11-07 18:22:11 +0530

    Complete code till LECTURE:70 - Selecting and Handling Elements using DOM

--- Practice/07-JS in The Browser/05-Guess My Number/script.js
+++ Practice/07-JS in The Browser/05-Guess My Number/script.js
@@ -1,2 +1,15 @@
 "use strict";
 console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = " Correct Number !!";
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 17;
+document.querySelector(".guess").value = 24;
+
+console.log(
+  document.querySelector(".number").textContent,
+  document.querySelector(".score").textContent
+  // document.querySelector(".guess").value;
+);
+
+console.log(document.querySelector(".guess").value);
