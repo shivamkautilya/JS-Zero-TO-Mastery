a65a0830ece1c601ae48858aa8bf8c8555ce32a1
Shivam Kautilya <57669336+shivamkautilya@users.noreply.github.com>
2022-11-09 18:17:20 +0530

    06-MODAL - declare const for selection of classes.

--- Practice/07-JS in The Browser/06 - Modal/script.js
+++ Practice/07-JS in The Browser/06 - Modal/script.js
@@ -1,1 +1,6 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector("overlay");
+const btnsOpenModal = document.querySelector(".show-modal");
+const btnCloseModal = document.querySelector(".close-modal");
