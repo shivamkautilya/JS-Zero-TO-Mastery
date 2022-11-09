0650ffa3e269edf638f61b9db71bd579b748a8fe
Shivam Kautilya <57669336+shivamkautilya@users.noreply.github.com>
2022-11-09 18:33:00 +0530

    06-Modal - add funtions to make work modal buttons

--- Practice/07-JS in The Browser/06 - Modal/script.js
+++ Practice/07-JS in The Browser/06 - Modal/script.js
@@ -1,6 +1,20 @@
 "use strict";
 
 const modal = document.querySelector(".modal");
-const overlay = document.querySelector("overlay");
-const btnsOpenModal = document.querySelector(".show-modal");
+const overlay = document.querySelector(".overlay");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
 const btnCloseModal = document.querySelector(".close-modal");
+//functions for buttons click
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden"), overlay.classList.add("hidden");
+};
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
