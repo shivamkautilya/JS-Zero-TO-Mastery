47fee93c80235b0263c0eae5c7054643a8524bef
Shivam Kautilya <57669336+shivamkautilya@users.noreply.github.com>
2022-11-10 17:14:23 +0530

    07-Pig-Game: declare constants and set default value to 0.

--- Practice/07-JS in The Browser/07-Pig-Game/script.js
+++ Practice/07-JS in The Browser/07-Pig-Game/script.js
@@ -1,1 +1,11 @@
-'use strict';
+"use strict";
+const score0 = document.getElementById("score--0");
+const score1 = document.getElementById("score--1");
+const dice = document.querySelector(".dice");
+
+//setting default values to 0
+score0.textContent = 0;
+score1.textContent = 0;
+dice.classList.add("hidden");
+
+//New Game reset button
