d2e6019c9a37ced1257b5dff0daea467463f92bb
Shivam Kautilya <57669336+shivamkautilya@users.noreply.github.com>
2022-11-10 18:08:10 +0530

    07-Pig-Game: Add function to rollDice and New Game button.

--- Practice/07-JS in The Browser/07-Pig-Game/script.js
+++ Practice/07-JS in The Browser/07-Pig-Game/script.js
@@ -1,11 +1,50 @@
 "use strict";
-const score0 = document.getElementById("score--0");
-const score1 = document.getElementById("score--1");
-const dice = document.querySelector(".dice");
+
+//variable/constant declarations
+const score0El = document.getElementById("score--0");
+const score1El = document.getElementById("score--1");
+const currentScore0El = document.getElementById("current--0");
+const currentScore1El = document.getElementById("current--1");
+const diceClassEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+let currentScore = 0;
+
+//FUNCTIONS
+const rollDice = function () {
+  //1. Generate dice number between 1-6
+  const diceNumber = Math.trunc(Math.random() * 6) + 1;
+  console.log(diceNumber);
+  //2.Display Dice Image in center
+  diceClassEl.classList.remove("hidden");
+  diceClassEl.src = `images/dice-${diceNumber}.png`;
+
+  //3. Show and add current score if dice != 1
+  if (diceNumber != 1) {
+    currentScore0El.textContent = diceNumber;
+  }
+};
+
+const newGame = function () {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  currentScore0El.textContent = 0;
+  currentScore1El.textContent = 0;
+  diceClassEl.classList.add("hidden");
+};
 
 //setting default values to 0
-score0.textContent = 0;
-score1.textContent = 0;
-dice.classList.add("hidden");
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceClassEl.classList.add("hidden");
 
 //New Game reset button
+// function newGame() {
+//   score0.textContent = 0;
+//   score1.textContent = 0;
+//   dice.classList.add("hidden");
+// }
+btnRoll.addEventListener("click", rollDice);
+btnNew.addEventListener("click", newGame);
+// btnHold.addEventListener("click");
