fe9b56a964020b3858d69582561daebe2b8c4ae3
Shivam Kautilya <57669336+shivamkautilya@users.noreply.github.com>
2022-11-11 18:41:42 +0530

    07:Pig Game - Game WInner IS NOW DECIDED...Version 1 completed.

--- Practice/07-JS in The Browser/07-Pig-Game/script.js
+++ Practice/07-JS in The Browser/07-Pig-Game/script.js
@@ -12,7 +12,7 @@ const btnHold = document.querySelector(".btn--hold");
 let currentScore = 0;
 let activePlayer = 0;
 let notActivePlayer = 1;
-
+let finalScore;
 let scores = [0, 0];
 
 //FUNCTIONS
@@ -30,39 +30,76 @@ const switchPlayer = function () {
     .querySelector(`.player--${notActivePlayer}`)
     .classList.remove("player--active");
 };
+//rolldice function
 const rollDice = function () {
-  //1. Generate dice number between 1-6
-  const diceNumber = Math.trunc(Math.random() * 6) + 1;
-  console.log(diceNumber);
-  //2.Display Dice Image in center
-  diceClassEl.classList.remove("hidden");
-  diceClassEl.src = `images/dice-${diceNumber}.png`;
+  if (scores[activePlayer] < 100) {
+    //1. Generate dice number between 1-6
+    const diceNumber = Math.trunc(Math.random() * 6) + 1;
+    // console.log(diceNumber);
+    //2.Display Dice Image in center
+    diceClassEl.classList.remove("hidden");
+    diceClassEl.src = `images/dice-${diceNumber}.png`;
+    //3. Show and add current score if dice != 1
+    //4. If score is below 100 then let it work
 
-  //3. Show and add current score if dice != 1
-  if (diceNumber != 1) {
-    currentScore += diceNumber;
-    // currentScore0El.textContent = currentScore;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
+    if (diceNumber != 1) {
+      currentScore += diceNumber;
+      // currentScore0El.textContent = currentScore;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
   } else {
-    switchPlayer();
+    declareWinner();
   }
 };
-
+function declareWinner() {
+  console.log("Game Has Ended.");
+  console.log(`Player ${activePlayer + 1} is winner.`);
+  diceClassEl.classList.add("hidden");
+  alert(`Player ${activePlayer + 1} is winner.`);
+}
 const btnHoldFunction = function () {
-  console.log("Hold button clicked");
+  // console.log("Hold button clicked");
   //1 Store currentscore into respected Player Score
   //verify if score = 100
-  if (activePlayer == 0) {
-    scores[0] += currentScore;
-    document.getElementById(`score--${activePlayer}`).textContent = scores[0];
-    switchPlayer();
-  } else if (activePlayer == 1) {
-    scores[1] += currentScore;
-    document.getElementById(`score--${activePlayer}`).textContent = scores[1];
-    switchPlayer();
+  if (scores[activePlayer] < 100) {
+    scores[activePlayer] += currentScore;
+    if (activePlayer == 0) {
+      // scores[0] += currentScore;
+      if (scores[0] >= 100) {
+        document.getElementById(`score--0`).textContent = scores[0];
+        document
+          .querySelector(".player--0")
+          .classList.add("player--winner", "name");
+        declareWinner();
+      } else {
+        document.getElementById(`score--${activePlayer}`).textContent =
+          scores[0];
+        switchPlayer();
+      }
+    } else if (activePlayer == 1) {
+      // scores[1] += currentScore;
+      if (scores[1] >= 100) {
+        document.getElementById(`score--1`).textContent = scores[1];
+        document
+          .querySelector(".player--1")
+          .classList.add("player--winner", "name");
+        declareWinner();
+      } else {
+        document.getElementById(`score--${activePlayer}`).textContent =
+          scores[1];
+        switchPlayer();
+      }
+    } else {
+      console.log(
+        "Unknown Error:activePlayer value is more than 1.It should be either 0 or 1."
+      );
+    }
+  } else {
+    declareWinner();
   }
-  //2 Switch to Next Player
 };
 
 const newGame = function () {
@@ -73,18 +110,15 @@ const newGame = function () {
   diceClassEl.classList.add("hidden");
   document.querySelector(".player--0").classList.add("player--active");
   document.querySelector(".player--1").classList.remove("player--active");
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove("player--winner", "name");
 };
 //setting default values to 0
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceClassEl.classList.add("hidden");
 
-//New Game reset button
-// function newGame() {
-//   score0.textContent = 0;
-//   score1.textContent = 0;
-//   dice.classList.add("hidden");
-// }
 btnRoll.addEventListener("click", rollDice);
 btnNew.addEventListener("click", newGame);
 btnHold.addEventListener("click", btnHoldFunction);
