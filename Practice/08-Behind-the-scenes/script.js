8b85ed761cf9ed0273a18c8e4f112eb6f88ae976
Shivam Kautilya <57669336+shivamkautilya@users.noreply.github.com>
2022-11-14 17:38:37 +0530

    Lecture 93: Scoping in Practice completed.

--- Practice/08-Behind-the-scenes/script.js
+++ Practice/08-Behind-the-scenes/script.js
@@ -1,1 +1,17 @@
-'use strict';
+"use strict";
+
+function ageCalculator(birthYear) {
+  if (birthYear < currentYear && birthYear > 1900) {
+    const myName = firstName;
+    const age = currentYear - birthYear;
+    return `${myName} your age is ${age}.`;
+  } else if (birthYear < 1900) {
+    console.log("Enter real birth year,1900 below not possible.");
+  } else {
+    alert("Please enter year less than current year.");
+  }
+}
+
+const firstName = "Shivam";
+const currentYear = new Date().getFullYear();
+console.log(ageCalculator(1890));
