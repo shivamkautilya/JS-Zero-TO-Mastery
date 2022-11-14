d4c5a69efa0aa62ce04e9e4191b29fad566fd0bc
Shivam Kautilya <57669336+shivamkautilya@users.noreply.github.com>
2022-11-14 19:57:40 +0530

    Update

--- Practice/08-Behind-the-scenes/script.js
+++ Practice/08-Behind-the-scenes/script.js
@@ -14,4 +14,23 @@ function ageCalculator(birthYear) {
 
 const firstName = "Shivam";
 const currentYear = new Date().getFullYear();
-console.log(ageCalculator(1890));
+console.log(ageCalculator(2005));
+
+//lecture 94
+// console.log(namePlease);
+// const namePlease = "shivam";
+
+//lecture 96: Using this keyword
+console.log(this); //return window from global object
+const createFullName = {
+  fullName: function () {
+    return `Your full name is ${this.firstName} ${this.lastName}.`;
+  },
+};
+
+const person = {
+  firstName: prompt("Enter your first name."),
+  lastName: prompt("Enter your last name."),
+};
+
+console.log(createFullName.fullName.call(person));
